a00dbfa76d79f70009ba8382de66dd24973e0c20
Andy Galdi <agaldi@zelusanalytics.com>
2023-02-06 10:26:53 -0500

    Write video segmentation code and instructions

---
+++ 03-cut_video.py
@@ -0,0 +1,27 @@
+from google.cloud import videointelligence_v1 as vi
+
+
+def detect_shot_changes(video_uri: str) -> vi.VideoAnnotationResults:
+    video_client = vi.VideoIntelligenceServiceClient()
+    features = [vi.Feature.SHOT_CHANGE_DETECTION]
+    request = vi.AnnotateVideoRequest(input_uri=video_uri, features=features)
+
+    print(f'Processing video: "{video_uri}"...')
+    operation = video_client.annotate_video(request)
+
+    return operation.result().annotation_results[0]  # Single video
+
+
+def print_video_shots(results: vi.VideoAnnotationResults):
+    shots = results.shot_annotations
+    print(f" Video shots: {len(shots)} ".center(40, "-"))
+    for i, shot in enumerate(shots):
+        t1 = shot.start_time_offset.total_seconds()
+        t2 = shot.end_time_offset.total_seconds()
+        print(f"{i+1:>3} | {t1:7.3f} | {t2:7.3f}")
+
+
+if __name__ == "__main__":
+    video_uri = "gs://slow-motion-videos/NDjokavicFH.mp4"
+    results = detect_shot_changes(video_uri)
+    print_video_shots(results)
--- README.md
+++ README.md
@@ -9,9 +9,22 @@ pyenv activate tennis-cv
 # Install requirements
 poetry install
 
+# Set up gcloud config
+gcloud config set account andy.galdi@gmail.com
+gcloud config set project cloudvisionproject-376918
+export GOOGLE_APPLICATION_CREDENTIALS=~/key.json
+
 # Run code
-python 2-determine-joints.py
+python 2-determine-joints.py # Will do pose estimation on a video and calculate joint angles
+
+# Cut video using CloudVisionAPI
+# Track racquet and ball
+# Determine contact point (and calculate joint angles at contact point)
+# Repeat for other segments of swing
+# Determine raquet speed (relative to max)
 ```
 
 # Notes
-High speed video can be found here: https://www.youtube.com/watch?v=9EgXxWYvlgk
+Slow-Mo Video found here: https://www.youtube.com/@LoveTennisOfficial
+Video Downloaded using this tool: https://youtubemp4.to/en27/
+CloudVision API instructions: https://codelabs.developers.google.com/codelabs/cloud-video-intelligence-python3#0
